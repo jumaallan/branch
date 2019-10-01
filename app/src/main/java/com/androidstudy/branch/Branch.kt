67677974dda83ea55b240b05b9babcf1002173d3
Juma Allan <allanjuma@gmail.com>
2019-10-01 15:26:44 +0300

    Adding Timber Initialization

--- app/src/main/java/com/androidstudy/branch/Branch.kt
+++ app/src/main/java/com/androidstudy/branch/Branch.kt
@@ -2,13 +2,18 @@ package com.androidstudy.branch
 
 import android.app.Application
 import androidx.annotation.Nullable
+import com.androidstudy.branch.settings.Settings
 import org.jetbrains.annotations.NotNull
 import timber.log.Timber
 
 class Branch : Application() {
 
+    lateinit var settings: Settings
+
     override fun onCreate() {
         super.onCreate()
+
+        settings = Settings(applicationContext)
 
         initTimber()
     }
