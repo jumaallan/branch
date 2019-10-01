ee86b2f98628356bcea0264c93f6ec838ee2cfd9
Juma Allan <allanjuma@gmail.com>
2019-10-01 15:07:31 +0300

    Toast Util - Extension Function

---
+++ app/src/main/java/com/androidstudy/branch/util/Util.kt
@@ -0,0 +1,13 @@
+package com.androidstudy.branch.util
+
+import android.app.Activity
+import android.widget.Toast
+
+private var toast: Toast? = null
+
+internal fun Activity.toast(message: CharSequence) {
+    toast?.cancel()
+    toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        .apply { show() }
+}
+
