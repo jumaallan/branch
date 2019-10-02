79eef30fa7e914908dbaaa2b3b0b0e7d91f32f60
Juma Allan <allanjuma@gmail.com>
2019-10-02 17:48:56 +0300

    Introducing Shimmering RecyclerView Effect

--- app/src/main/java/com/androidstudy/branch/ui/views/DashboardActivity.kt
+++ app/src/main/java/com/androidstudy/branch/ui/views/DashboardActivity.kt
@@ -3,11 +3,14 @@ package com.androidstudy.branch.ui.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.androidstudy.branch.R
+import kotlinx.android.synthetic.main.content_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        shimmerRecyclerView.showShimmerAdapter()
     }
 }
