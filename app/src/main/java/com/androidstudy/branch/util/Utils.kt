a02ae25d948a7f628c4e513bd19a0b177667344e
Juma Allan <allanjuma@gmail.com>
2019-10-03 07:54:36 +0300

    Setting up timestamp to time conversion

--- app/src/main/java/com/androidstudy/branch/util/Utils.kt
+++ app/src/main/java/com/androidstudy/branch/util/Utils.kt
@@ -1,17 +1,23 @@
 package com.androidstudy.branch.util
 
 import android.annotation.SuppressLint
+import android.text.format.DateUtils
 import java.text.SimpleDateFormat
+import java.util.*
+
 
 internal class Utils {
 
     companion object {
 
         @SuppressLint("SimpleDateFormat")
-        fun getFormattedUpdateTime(dateToConvert: String): String {
-            val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm")
-            val date = dateFormat.parse(dateToConvert)
-            return String.format(date.toString())
+        fun getFormattedUpdateTime(dateToConvert: String): CharSequence {
+            val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            sdf.timeZone = TimeZone.getTimeZone("GMT")
+            val time = sdf.parse(dateToConvert)!!.time
+            val now = System.currentTimeMillis()
+            return DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS)
         }
+
     }
 }
