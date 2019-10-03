844af0ed60483cc5d420317b6a3f124cc16e1993
Juma Allan <allanjuma@gmail.com>
2019-10-03 12:02:54 +0300

    Introducing Room and Room Entities

---
+++ app/src/main/java/com/androidstudy/branch/data/entities/Thread.kt
@@ -0,0 +1,11 @@
+package com.androidstudy.branch.data.entities
+
+class Thread(
+    var id: Int,
+    var thread_id: Int,
+    var user_id: String,
+    var body: String,
+    var timestamp: String,
+    var status: String,
+    var agent_id: String
+)
