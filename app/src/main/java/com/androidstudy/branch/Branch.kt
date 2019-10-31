04a3efbbd73d843cf0d35ad6ba5280050eeeea1f
Juma Allan <allanjuma@gmail.com>
2019-10-31 12:27:26 +0300

    Introducing Firebase - Crashlytics, Performance and Anayltics

--- app/src/main/java/com/androidstudy/branch/Branch.kt
+++ app/src/main/java/com/androidstudy/branch/Branch.kt
@@ -1,9 +1,11 @@
 package com.androidstudy.branch
 
 import android.app.Application
+import android.os.Build
 import androidx.annotation.Nullable
 import com.androidstudy.branch.di.*
 import com.androidstudy.branch.settings.Settings
+import com.androidstudy.branch.util.CrashlyticsTree
 import com.facebook.stetho.Stetho
 import org.jetbrains.annotations.NotNull
 import org.koin.android.ext.android.startKoin
@@ -11,12 +13,14 @@ import timber.log.Timber
 
 class Branch : Application() {
 
+    private var mInstance: Branch? = null
     lateinit var settings: Settings
 
     override fun onCreate() {
         super.onCreate()
 
         settings = Settings(applicationContext)
+        mInstance = this
 
         initKoin()
         initTimber()
@@ -24,7 +28,9 @@ class Branch : Application() {
     }
 
     private fun initStetho() {
-        Stetho.initializeWithDefaults(this)
+        if (!isRoboUnitTest() && BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
     }
 
     private fun initKoin() {
@@ -35,12 +41,20 @@ class Branch : Application() {
     }
 
     private fun initTimber() {
-        Timber.plant(object : Timber.DebugTree() {
-            @Nullable
-            override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
-                return super.createStackElementTag(element) + ":" + element.lineNumber
-            }
-        })
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        } else {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+
+    private fun isRoboUnitTest(): Boolean {
+        return "robolectric" == Build.FINGERPRINT
     }
 
 }
