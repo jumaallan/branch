dcc41ba1f9494280a015b772ffe5b2c6f806f7a4
Juma Allan <allanjuma@gmail.com>
2019-11-01 04:25:25 +0300

    A little housekeeping

--- app/src/main/java/com/androidstudy/branch/data/dao/ChatDao.kt
+++ app/src/main/java/com/androidstudy/branch/data/dao/ChatDao.kt
@@ -1,15 +1,18 @@
 package com.androidstudy.branch.data.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
 import com.androidstudy.branch.data.entities.ChatMessage
+import com.androidstudy.branch.data.entities.StockMessage
 
 @Dao
 interface ChatDao : BaseDao<ChatMessage> {
 
-//    @Query("SELECT * FROM ChatMessage WHERE thread_id =:thread_id")
-//    fun fetchMessagesPerThread(thread_id: String): LiveData<List<ChatMessage>>
-//
-//    @Query("SELECT * FROM StockMessage")
-//    fun fetchStockMessages(): LiveData<List<StockMessage>>
+    @Query("SELECT * FROM ChatMessage WHERE thread_id =:thread_id")
+    fun fetchMessagesPerThread(thread_id: String): LiveData<List<ChatMessage>>
+
+    @Query("SELECT * FROM StockMessage")
+    fun fetchStockMessages(): LiveData<List<StockMessage>>
 
 }
